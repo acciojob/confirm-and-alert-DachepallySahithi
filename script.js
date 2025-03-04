d18a8574f7cedd3159c2a08fa0358d888a8f3327
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-03-04 14:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Ask for confirmation
+const userResponse = confirm("Do you want to proceed?");
+
+// Check user response
+if (userResponse) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
